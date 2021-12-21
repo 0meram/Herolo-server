1d3611e6fb2b1863e1163085dc12a5a6741ca325
omer ram <0meram95@gmail.com>
2021-12-21 15:54:57 +0200

    server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+// app.use(cors());
+// app.use("/search", require("./routes/search"));
+
+app.get("/", (req, res) => {
+	res.send(`<h1>Herolo Server i have automation</h1>`);
+});
+
+app.listen(PORT, () => {
+	console.log(`im ready to work on port ${PORT}`);
+});
